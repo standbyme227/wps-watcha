e985d7b320cc6e9bde3d155d390c0f2f07f39162
ShinJongMin <standbyme227@gmail.com>
2018-04-01 09:47:55 +0900

    domain을 붙이기위해서 nginx의 server-name과 allowed host를 변경

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -8,9 +8,10 @@ set_config(secrets, module_name=__name__, start=True)
 WSGI_APPLICATION = 'config.wsgi.production.application'
 
 ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '.elasticbeanstalk.com',
+    # 'localhost',
+    # '127.0.0.1',
+    # '.elasticbeanstalk.com',
+    '.justdo2t.com',
 ]
 
 INSTALLED_APPS += [
