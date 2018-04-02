476dc821792eb968e1e73a4b61e3364f895b94c5
Shin Dongjin <djshin2000@gmail.com>
2018-04-02 19:36:44 +0900

    email 로그인을 위한 User app의 model, manager 작성 및 admin 수정

--- app/members/models.py
+++ app/members/models.py
@@ -1,8 +1,33 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from movie.models import Movie
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, nickname, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname=nickname
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
     movies = models.ManyToManyField(Movie, verbose_name='영화 목록', blank=True)
-    pass
+    email = models.EmailField(verbose_name='email', max_length=255, unique=True)
+    nickname = models.CharField(verbose_name='nickname', max_length=20, blank=False, unique=True, default='')
+    img_profile = models.ImageField(upload_to='user', blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nickname']
+
+    def __str__(self):
+        return self.email
+
