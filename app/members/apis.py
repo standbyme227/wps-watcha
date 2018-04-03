a357a1b58650e85e459941e012ba0989233c261e
ShinJongMin <standbyme227@gmail.com>
2018-04-03 13:39:56 +0900

    userlist serializer(APIView) 구현

--- app/members/apis.py
+++ app/members/apis.py
@@ -1,8 +1,13 @@
+from django.contrib.auth import get_user_model
+from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .serializers import AccessTokenSerializer, UserSerializer
+
+
+User = get_user_model()
 
 
 class AuthTokenForFacebookAccessTokenView(APIView):
@@ -13,7 +18,21 @@ class AuthTokenForFacebookAccessTokenView(APIView):
         token, _ = Token.objects.get_or_create(user=user)
         data = {
             'token': token.key,
-            'user' : UserSerializer(user).data,
+            'user': UserSerializer(user).data,
         }
         return Response(data)
+
+
+class UserList(APIView):
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
