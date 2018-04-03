cfe0cba73333c325fa4ce03b9323741270014c06
ShinJongMin <standbyme227@gmail.com>
2018-04-03 09:29:00 +0900

    user모델에 img_profile, nickname 필드를 추가

--- app/members/models.py
+++ app/members/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 from movie.models import Movie
 
 
 class User(AbstractUser):
+    img_profile = models.ImageField('프로필 사진', upload_to='user', blank=True)
+    nickname = models.Charfield('별명', blank=True)
+
     movies = models.ManyToManyField(Movie, verbose_name='영화 목록', blank=True)
     pass
