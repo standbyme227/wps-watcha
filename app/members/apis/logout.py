1221b7a883da6189aa5affdbd87bea3de1a4eb30
ShinJongMin <standbyme227@gmail.com>
2018-04-04 15:21:41 +0900

    여러가지 package화 및 facebook 가입이후 로그인시 적용이 안되는 부분을 수정

---
+++ app/members/apis/logout.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from rest_framework import status, permissions
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+User = get_user_model()
+
+__all__ = (
+    'LogoutView',
+)
+
+
+class LogoutView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, format=None):
+        request.user.token.delete()
+        return Response(status=status.HTTP_200_OK)
---
+++ app/members/serializers/email_serializer.py
@@ -0,0 +1,40 @@
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.compat import authenticate as rest_authenticate
+
+User = get_user_model()
+
+__all__ = (
+    'EmailAuthTokenSerializer',
+)
+
+
+class EmailAuthTokenSerializer(serializers.Serializer):
+    email = serializers.CharField(label=_("Email"))
+    password = serializers.CharField(
+        label=_("Password"),
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = rest_authenticate(request=self.context.get('request'),
+                                     email=email, password=password)
+
+            # The authenticate call simply returns None for is_active=False
+            # users. (Assuming the default ModelBackend authentication
+            # backend.)
+            if not user:
+                msg = _('Unable to log in with provided credentials.')
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = _('Must include "email" and "password".')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
