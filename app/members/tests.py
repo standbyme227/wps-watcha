bc8d17e25b0cbd7c20a43e9872ce4de8fd91daff
Shin Dongjin <djshin2000@gmail.com>
2018-04-04 15:28:32 +0900

    user test 코드 작성 시작

--- app/members/tests.py
+++ app/members/tests.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+User = get_user_model()
+
+
+class EmailAccountsTest(TestCase):
+    def setup(self):
+        self.test_user = User.objects.create_user('20djshin@naver.com', '네이버계정', 'testpassword')
+        pass
