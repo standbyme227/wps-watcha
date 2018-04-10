f2cd5ce10b90836c5ea8fc5ee1accb80d0be194e
Shin Dongjin <djshin2000@gmail.com>
2018-04-11 00:40:52 +0900

    user_serializer에 중개모델 시리얼라이즈 하는 샘플코드 추가

--- app/members/serializers/user_serializer.py
+++ app/members/serializers/user_serializer.py
@@ -1,13 +1,46 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
+# from movie.models import UserToMovie
+
 User = get_user_model()
 
 __all__ = (
+    # 'UserToMovieSerializer',
     'UserSerializer',
     'UserDetailSerializer',
     'UserEmailSerializer',
 )
+
+
+# class UserToMovieSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = UserToMovie
+#         fields = (
+#             'movie',
+#             'user_want_movie',
+#             'user_watched_movie',
+#             'rating',
+#             'comment'
+#         )
+#
+#
+# class UserSerializer(serializers.ModelSerializer):
+#     interesting_movie_list = UserToMovieSerializer(many=True, read_only=True)
+#
+#     class Meta:
+#         model = User
+#         fields = (
+#             'pk',
+#             'email',
+#             'username',
+#             'nickname',
+#             'img_profile',
+#             'first_name',
+#             'last_name',
+#             'interesting_movie_list'
+#         )
+#         read_only_fields = ('pk', 'username',)
 
 
 class UserSerializer(serializers.ModelSerializer):
