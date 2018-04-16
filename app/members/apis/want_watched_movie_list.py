ae782de6e2ffb7866ea6e399ddd422827cb2349b
Shin Dongjin <djshin2000@gmail.com>
2018-04-16 16:00:30 +0900

    want, watched movie list API에 로그인 사용자의 영화평가 정보도 포함되도록 수정

--- app/members/apis/want_watched_movie_list.py
+++ app/members/apis/want_watched_movie_list.py
@@ -36,19 +36,5 @@ class WantWatchedMovieListView(APIView):
         for item in user_to_movie:
             movie_list.append(item.movie)
 
-        serializer = WantWatchedMovieListSerializer(movie_list, many=True)
+        serializer = WantWatchedMovieListSerializer(movie_list, many=True, context={'login_user': request.user})
         return Response(serializer.data)
-
-    # def put(self, request, pk, format=None):
-    #     if request.data['user_want_movie'] == request.data['user_watched_movie']:
-    #         err = {"error": ["user_want_movie' 와 'user_watched_movie'의 value는 둘 다 '참'이거나 '거짓'일 수 없습니다."]}
-    #         return Response(data=err, status=status.HTTP_400_BAD_REQUEST)
-    #
-    #     user = get_object_or_404(User, pk=pk)
-    #     user_to_movie = get_object_or_404(UserToMovie, user=user, movie=request.data['movie'])
-    #     serializer = UserToMovieUpdateSerializer(user_to_movie, data=request.data)
-    #
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- app/movie/serializers/movie_serializer.py
+++ app/movie/serializers/movie_serializer.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
 from ..serializers import GenreSerializer
-from ..models import Movie
+from ..models import Movie, UserToMovie
+from movie.serializers.user_to_movie_serializer import UserToMovieWantWatchedListSerializer
 
 __all__ = (
     'MovieListSerializer',
@@ -17,7 +18,8 @@ class MovieListSerializer(serializers.ModelSerializer):
 
 class WantWatchedMovieListSerializer(MovieListSerializer):
     genre = GenreSerializer(many=True)
-
+    # movie_id = serializers.IntegerField(source='id')
+    login_user_checked = serializers.SerializerMethodField()
     # 장르의 텍스트만 리스트형태로 전달하고 싶을 때에는 아래처럼 SlugRelatedField를 사용
     # genre = serializers.SlugRelatedField(many=True, read_only=True, slug_field='genre')
 
@@ -32,4 +34,16 @@ class WantWatchedMovieListSerializer(MovieListSerializer):
             'poster_image',
             'genre',
             'running_time',
+            'login_user_checked',
         )
+
+    def get_login_user_checked(self, obj):
+        items = UserToMovie.objects.filter(user=self.context['login_user'], movie=obj)
+        if len(items) == 1:
+            for item in items:
+                serializer = UserToMovieWantWatchedListSerializer(item)
+                return serializer.data
+        elif len(items) == 0:
+            return {'no-data': 'does not exist.'}
+        else:
+            return {'error': 'Problems with data consistency'}
