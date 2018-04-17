081b0d1a156e9d6ec950e83814d02b9b6626acc3
ShinJongMin <standbyme227@gmail.com>
2018-04-17 15:13:21 +0900

    배포 후 choice field display 수정해서 한번 더 수정

--- app/movie/urls/apis/box_office.py
+++ app/movie/urls/apis/box_office.py
@@ -10,7 +10,7 @@ app_name = 'box-office'
 
 urlpatterns = [
     path('name-list/', MovieBoxofficeRankingNameListView.as_view(), name='box-office-ranking-name-list'),
-    path('five-list/', MovieBoxofficeRankingFiveListView.as_view(), name='box-office-ranking-name-list'),
-    path('', MovieBoxofficeRankingListView.as_view(), name='box-office-ranking-name-list'),
+    path('five-list/', MovieBoxofficeRankingFiveListView.as_view(), name='box-office-five-list'),
+    path('', MovieBoxofficeRankingListView.as_view(), name='box-office-list'),
 
 ]
