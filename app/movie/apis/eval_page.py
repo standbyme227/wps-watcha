cec2622210fa5fe026966d7246130cea2a15920a
ShinJongMin <standbyme227@gmail.com>
2018-04-17 14:21:09 +0900

    다시 배포준비

--- app/movie/apis/eval_page.py
+++ app/movie/apis/eval_page.py
@@ -29,7 +29,7 @@ class EvalWatchaRatingTopMovieListView(APIView):
     def get(self, request, format=None):
         if not request.user.is_anonymous:
             movie = Movie.objects.\
-                exclude(interested_user_list__token=request.user.token).filter(rating_avg__gte=4.3).order_by('?')
+                exclude(interested_user_list__token=request.user.auth_token).filter(rating_avg__gte=4.3).order_by('?')
         else:
             movie = Movie.objects.filter(rating_avg__gte=4.3).order_by('?')
         movie_list = []
@@ -51,7 +51,7 @@ class EvalTagMovieListView(APIView):
     def get(self, request, format=None):
         if not request.user.is_anonymous:
             movie = Movie.objects.\
-                exclude(interested_user_list__token=request.user.token).filter(tag__tag=self.TAG).order_by('?')
+                exclude(interested_user_list__token=request.user.auth_token).filter(tag__tag=self.TAG).order_by('?')
         else:
             movie = Movie.objects.filter(tag__tag=self.TAG).order_by('?')
         # movie = Movie.objects.filter(tag__tag=self.TAG)
@@ -73,7 +73,7 @@ class EvalGenreMovieListView(APIView):
     def get(self, request, format=None):
         if not request.user.is_anonymous:
             movie = Movie.objects.\
-                exclude(interested_user_list__token=request.user.token).filter(genre__genre=self.GENRE).order_by('?')
+                exclude(interested_user_list__token=request.user.auth_token).filter(genre__genre=self.GENRE).order_by('?')
         else:
             movie = Movie.objects.filter(genre__genre=self.GENRE).order_by('?')
         # movie = Movie.objects.filter(genre__genre=self.GENRE)
