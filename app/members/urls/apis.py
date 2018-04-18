af86a4c35d96647518979105dbf9e92b0007c093
ShinJongMin <standbyme227@gmail.com>
2018-04-18 10:38:17 +0900

    my page를 위해서 수정중

--- app/members/urls/apis.py
+++ app/members/urls/apis.py
@@ -23,7 +23,9 @@ urlpatterns = [
     path('facebook-auth-token/', AuthTokenForFacebookAccessTokenView.as_view(), name='facebook-login'),
     path('email-auth-token/', AuthTokenForEmailView.as_view(), name='email-login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
     path('<int:pk>/want-movie/', WantWatchedMovieListView.as_view(want_movie=True), name='want-movie'),
     path('<int:pk>/watched-movie/', WantWatchedMovieListView.as_view(watched_movie=True), name='watched-movie'),
+    # path('')
 ]
 
