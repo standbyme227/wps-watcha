dbb416cc29d5fc68cf8231537d30620dee7c4b94
Shin Dongjin <djshin2000@gmail.com>
2018-04-20 11:38:46 +0900

    필요없는 api view 파일 삭제

--- app/movie/apis/__init__.py
+++ app/movie/apis/__init__.py
@@ -3,7 +3,6 @@ from .eval_page import *
 from .main_page import *
 from .movie_detail import *
 from .movie_list import *
-from .movie_to_member_list import *
 from .search_movie import *
 from .user_to_movie_create import *
 from .user_to_movie_list import *
