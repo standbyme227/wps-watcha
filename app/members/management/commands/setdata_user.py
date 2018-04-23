ff369b9dbc8880791b34c04f219757da366ec2e8
Shin Dongjin <djshin2000@gmail.com>
2018-04-23 13:30:38 +0900

    setdata_user 작성(img_profile저장은 제외)

--- app/members/management/commands/setdata_user.py
+++ app/members/management/commands/setdata_user.py
@@ -18,16 +18,16 @@ class Command(BaseCommand):
                 'nickname': '탱구',
                 'password': 'abc123456789',
             },
-            # {
-            #     'email': 'nana@test.com',
-            #     'nickname': '나나',
-            #     'password': 'abc123456789',
-            # },
-            # {
-            #     'email': 'omg@test.com',
-            #     'nickname': 'oh my girl',
-            #     'password': 'abc123456789',
-            # },
+            {
+                'email': 'nana@test.com',
+                'nickname': '나나',
+                'password': 'abc123456789',
+            },
+            {
+                'email': 'omg@test.com',
+                'nickname': 'oh my girl',
+                'password': 'abc123456789',
+            },
         ]
         for user in user_data:
             if not User.objects.filter(email=user['email']).exists():
@@ -36,16 +36,16 @@ class Command(BaseCommand):
                     nickname=user['nickname'],
                     password=user['password'],
                 )
-                if user.img_profile:
-                    user.img_profile.delete()
-                img_dir = '/home/djshin2000/Downloads/img_profile'
-                file_path = os.path.join(img_dir, 'taeyeon_1.jpg')
-                with open(file_path, 'r') as f:
-                    contents = f.read()
-                    file_name = 'user_{user_id}.{ext}'.format(
-                        user_id=user.id,
-                        ext=get_buffer_ext(contents)
-                    )
-                    user.img_profile.save(file_name, File(contents))
+                # if user.img_profile:
+                #     user.img_profile.delete()
+                # img_dir = '/home/djshin2000/Downloads/img_profile'
+                # file_path = os.path.join(img_dir, 'taeyeon_1.jpg')
+                # with open(file_path, 'r') as f:
+                #     contents = f.read()
+                #     file_name = 'user_{user_id}.{ext}'.format(
+                #         user_id=user.id,
+                #         ext=get_buffer_ext(contents)
+                #     )
+                #     user.img_profile.save(file_name, File(contents))
         print('connection.queries: ', len(connection.queries))
         self.stdout.write(self.style.SUCCESS('Success: setdata_user command'))
