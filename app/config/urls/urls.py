ad9e36f88462269f584fa58681d17c3acfb2df21
ShinJongMin <standbyme227@gmail.com>
2018-04-24 18:28:53 +0900

    음....admin을...

--- app/config/urls/urls.py
+++ app/config/urls/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('project-admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('movie/', include('movie.urls.views'))
 ]
